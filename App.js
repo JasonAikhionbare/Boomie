c953afb6b0b0ae4634d6cf070801225dbe569d15
Jason Aikhionbare <JAikhionbare@brownthomas.ie>
2021-08-18 12:50:45 +0100

    Further changes to App.js file and the TextAnimator.js file. Added parameter for the opening text as well as additional text to test the animation later

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TextAnimator from './components/TextAnimation';
 
 export default function App() {
 
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <TextAnimator content= 'Helping You Stay Connected With Your Chosen Family.  BOOMIE' 
+      textStyle={styles.textStyle}
+      style={styles.containerStyle}
+      />
+
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -12,7 +23,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#8a02b2',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 8,
+    paddingTop: Constants.statusBarHeight
   },
+  container: {},
+  textStyle: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    fontFamily: 'Times New Roman',
+    MarginBottom: 14
+  }
 });
-
-
